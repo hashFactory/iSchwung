3c34175e217b95267189543f70d236ac1170fee5
hashFactory <15621745+hashFactory@users.noreply.github.com>
2026-06-10 19:04:55 +0200

    Initial Commit

---
+++ iSchwung/iSchwungApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSchwungApp.swift
+//  iSchwung
+//
+//  Created by Tristan on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct iSchwungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
